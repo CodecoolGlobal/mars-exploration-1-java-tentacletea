48a52635f6fcbe96be34d7a1a6d1c4262d502b4f
raphaelloner <raphael.loner@gmail.com>
2023-03-15 14:04:03 +0100

    implemented ShapeGenerator class and getDummyMap funcion

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -3,6 +3,7 @@ package com.codecool.marsexploration;
 import com.codecool.marsexploration.data.Coordinate;
 import com.codecool.marsexploration.data.MapConfig;
 import com.codecool.marsexploration.data.MapSize;
+import com.codecool.marsexploration.logic.ShapeGenerator;
 
 import java.util.*;
 
@@ -10,7 +11,7 @@ public class Application {
     public static void main(String[] args) {
         String outputPath = "src/main/resources/exploration.map";
         int mapWidth = 50;
-
+        Random random = new Random();
         List<Integer> mountains = new ArrayList<>();
         List<Integer> pits = new ArrayList<>();
         mountains.add(10);
@@ -23,8 +24,10 @@ public class Application {
         int minerals = 5;
 
         MapConfig mapConfig = new MapConfig(outputPath, mapWidth, mountains, pits, water, minerals);
+        ShapeGenerator shapegenerator = new ShapeGenerator(mapConfig.mapWidth());
 
-        MapGenerater mapGenerater = new MapGenerator(mapConfig);
-        mapGenerater.generate();
+        //shapegenerator.run(mapConfig.mountains(),mapConfig.pits(),random);
+        //MapGenerator mapGenerator = new MapGenerator(mapConfig);
+       // mapGenerator.generate();
     }
 }
