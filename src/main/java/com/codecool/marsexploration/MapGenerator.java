01fff60925b5a498bbc17bed9b9cdbe5f23d9766
tentacletea <c98.william@gmail.com>
2023-03-15 10:27:29 +0100

    add mapgenerator class

---
+++ src/main/java/com/codecool/marsexploration/MapGenerator.java
@@ -0,0 +1,13 @@
+package com.codecool.marsexploration;
+
+import com.codecool.marsexploration.data.MapConfig;
+
+public class MapGenerator {
+    private final MapConfig mapConfig;
+    public MapGenerator(MapConfig mapConfig) {
+        this.mapConfig = mapConfig;
+    }
+    public void generate () {
+
+    }
+}
